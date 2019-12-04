de203bfca23f26091f929d5b3b6c44d4d84e4f4d
FarjanaParveen <farjanaparveena@syncfusion.com>
2019-12-04 12:58:19 +0530

    DataGrid export to excel in VB .NET?

---
+++ WPF/Application.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic
+#Region "Copyright Syncfusion Inc. 2001 - 2015"
+' Copyright Syncfusion Inc. 2001 - 2015. All rights reserved.
+' Use of this code is subject to the terms of our license.
+' A copy of the current license can be obtained at any time by e-mailing
+' licensing@syncfusion.com. Any infringement will be prosecuted under
+' applicable laws. 
+#End Region
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports System.Windows
+
+Namespace ExportingDemo
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+		Public Sub New()
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Syncfusion.Licensing.DemoCommon.FindLicenseKey())
+		End Sub
+	End Class
+End Namespace
